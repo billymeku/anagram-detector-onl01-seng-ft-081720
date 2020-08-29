67c143a3951a22d1d8928908f779be9c626ae433
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-29 03:57:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,14 +6,14 @@ class Anagram
     @word = word 
   end 
   
-  def match(words)
-    anagram = []
-    words.each do |wordy_words|
-      if wordy_words.split("").sort == word.split("").sort
-        anagram << wordy_words
-      end
-    end
-    anagram
-  end
-end 
+#   def match(words)
+#     anagram = []
+#     words.each do |wordy_words|
+#       if wordy_words.split("").sort == word.split("").sort
+#         anagram << wordy_words
+#       end
+#     end
+#     anagram
+#   end
+# end 
 
