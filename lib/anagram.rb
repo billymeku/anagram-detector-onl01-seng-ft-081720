9fdbb5a63f81f46683f8a26f58949128c085df09
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-29 03:36:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,10 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
-  @@all = []
+
   def initialze(word)
     @word = word 
   end 
-  
-   def self.all
-    @@all
-  end
   
   def self.match(array)
     word = self.new
