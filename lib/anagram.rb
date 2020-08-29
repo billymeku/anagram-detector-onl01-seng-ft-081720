65cbe94ac62f098f4527c411af170de5753f6022
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-29 03:56:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,30 +1,11 @@
-# # Your code goes here!
-# class Anagram
-#   attr_accessor :word
-
-#   def initialze(word)
-#     @word = word 
-#   end 
-  
-#   def match(words)
-#     anagram = []
-#     words.each do |wordy_words|
-#       if wordy_words.split("").sort == word.split("").sort
-#         anagram << wordy_words
-#       end
-#     end
-#     anagram
-#   end
-# end 
-
-
+# Your code goes here!
 class Anagram
   attr_accessor :word
 
-  def initialize(word)
-    @word = word
-  end
-
+  def initialze(word)
+    @word = word 
+  end 
+  
   def match(words)
     anagram = []
     words.each do |wordy_words|
@@ -34,4 +15,5 @@ class Anagram
     end
     anagram
   end
-end
+end 
+
